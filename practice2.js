fba81018a5993a54687d5b4d93e8bd1aa6e5bb78
Thea Villanueva <tei.villanueva@gmail.com>
2022-01-28 20:35:06 -0800

    practice-2

---
+++ practice2.js
@@ -0,0 +1,38 @@
+// Print odds 1-20
+// for (let i = 1; i < 20; i++){
+//     if (i % 2 === 1){
+//         console.log(i);
+//     }
+// };
+
+// Using a loop write code that will console.log all of the odd values from 1 up to 20.
+// Decreasing Multiples of 3
+ 
+    // for (let i = 20; i >= 1; i--){
+    //     if (i % 2 === 1 && i % 3 === 0){    
+    //         console.log(i)
+    //     }
+    // }
+
+
+// Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+// Print the sequence
+
+for (let i = 100; i >= 0; i-=1.3{
+    if ( i % 3 === 0){
+        console.log(i)
+    }
+}
+
+
+// Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+
+for (let i = 4; i >-4; i-=1.5){
+    console.log(i)
+}
+
+
+// Sigma
+// Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
+// Factorial
+// Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
